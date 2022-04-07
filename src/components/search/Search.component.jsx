7f51c52955bc1905578a8088d16d8d3235751a1b
Tony Manning <armanning253@gmail.com>
2022-04-06 23:49:57 -0700

    add modal for mic button on search bar, add dropdown functionality for other search bar buttons

--- src/components/search/Search.component.jsx
+++ src/components/search/Search.component.jsx
@@ -15,6 +15,12 @@ const Search = ({ onTermSubmit }) => {
 
     const [term, setTerm] = useState('web development');
     const [openMenu, setOpenMenu] = useState(false);
+    const [overlay, setOverlay] = useState(false);
+    const [modal, setModal] = useState(false);
+    const [videoCreateMenu, setVideoCreateMenu] = useState(false);
+    const [appMenu, setAppMenu] = useState(false);
+
+
 
     useEffect(() => (onTermSubmit(term)), []); //<--searchs default term
 
@@ -24,13 +30,32 @@ const Search = ({ onTermSubmit }) => {
     };
     const handleMenuSlide = () => {
         setOpenMenu(!openMenu);
+        if (overlay == openMenu) { handleOverlay() }
+    }
+    const handleOverlay = () => {
+        setOverlay(!overlay);
+    }
+    const handleModal = () => {
+        handleOverlay()
+        setModal(!modal)
+    }
+    const handleVideoCreateMenu = () => {
+        handleOverlay()
+        setVideoCreateMenu(!videoCreateMenu)
+    }
+    const handleAppMenu = () => {
+        handleOverlay()
+        setAppMenu(!appMenu)
     }
 
-
     return (
+
         <div className='searchContainer'>
+
+            {/* -------- MAIN MENU ICON ----------- */}
             <BsList onClick={() => { handleMenuSlide() }} className='searchBars searchIcons' />
 
+            {/* -------- MAIN SLIDE MENU ----------- */}
             <div className={openMenu ? "searchMenu active" : "searchMenu"}>
                 <span>
                     <BsList onClick={() => { handleMenuSlide() }} className='searchBars searchIcons' />
@@ -42,13 +67,16 @@ const Search = ({ onTermSubmit }) => {
 
                 <MenuBar />
             </div>
-            <div className={openMenu ? 'overlay' : ''} onClick={() => { handleMenuSlide() }}>overlay</div> {/* page overlay when model open*/}
 
+            {/* -------- OVERLAY DIV----------- */}
+            <div className={overlay ? 'overlay' : ''} ></div>
+
+            {/* -------- YOUTUBE LOGO ICON ----------- */}
             <Link to="/" className='toolTip'>
                 <div className='logo'><FaYoutube className='logoIcon searchIcons' />YouTube</div>
                 <p className='toolTipText'>YouTube Home</p>
             </Link>
-
+            {/* -------- SEARCH INPUT ----------- */}
             <form onSubmit={handleSubmit} >
                 <input
                     type="text"
@@ -58,36 +86,68 @@ const Search = ({ onTermSubmit }) => {
                 />
                 <BsX className='deleteIcon searchIcons' />
 
+                {/* -------- SEARCH ICON ----------- */}
                 <div className='toolTip'>
                     <FaSearch className='searchIcon searchIcons' />
                     <p className='toolTipText'>Search</p>
                 </div>
 
                 <div className='toolTip'>
-                    <FaMicrophone className='microphone searchIcons' />
+
+                    {/* -------- MICROPHONE ICON ----------- */}
+                    <FaMicrophone
+                        onClick={() => { handleModal() }}
+                        className='microphone searchIcons' />
+
                     <p className='toolTipText'>Search with your voice</p>
+
+                    <div className={modal ? 'modal active' : 'modal'}> {/* MODAL */}
+                        <div className='modalHeader'>
+                            <div className='modalTitle'>
+                                <h2>Search with your voice</h2>
+                            </div>
+                            <div className='modalExitBtn' onClick={handleModal}>
+                                <BsX />
+                            </div>
+                        </div>
+
+                        <p>To search by voice, go to your browser settings and allow access to microphone</p>
+                        <div className='modalMicIcon'>
+                            <FaMicrophone />
+                        </div>
+                    </div>
+
                 </div>
 
-            </form>
+            </form >
+            {/* -------- ICON GROUPING ----------- */}
             <div className='iconGroup'>
 
-
-                <div className='toolTip'>
-                    <BiVideoPlus className='videoIcon searchIcons ' />
+                {/* -------- VIDEO CREATION ICON ----------- */}
+                {/* <div className='toolTip'>
+                    <BiVideoPlus className='videoIcon searchIcons ' onClick={handleVideoCreateMenu} />
                     <p className='toolTipText'>Create</p>
-                </div>
-
+                    <div className={videoCreateMenu ? 'dropdown active' : 'dropdown'} > create videos</div>
+                </div> */}
+                {/* -------- GRID APPS ICON ----------- */}
                 <div className='toolTip'>
-                    <BsGrid3X3Gap className='searchIcons' />
+                    <BsGrid3X3Gap className='searchIcons' onClick={handleAppMenu} />
                     <p className='toolTipText'>YouTube apps</p>
+                    <div className={appMenu ? 'dropdown active' : 'dropdown'}></div>
                 </div>
+                {/* -------- 3 VERTICAL DOTS ICON ----------- */}
 
-                <div className='toolTip'>
+
+
+                {/* -------- NOTIFICATION BELL ICON ----------- */}
+                {/* <div className='toolTip'>
                     <BsBell className='searchIcons' />
                     <p className='toolTipText'>Notifications</p>
-                </div>
-
-                <img src="#" alt="pic" />
+                </div> */}
+                {/* -------- SIGN IN BUTTON ----------- */}
+                <button className='signInBtn'>SIGN IN</button>
+                {/* -------- PROFILE ----------- */}
+                {/* <img src="#" alt="pic" /> */}
             </div>
         </div >
     )
